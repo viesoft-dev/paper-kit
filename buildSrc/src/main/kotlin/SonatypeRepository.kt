05d6accab3f5acbf7c4834ac331677ce6b088741
viktorhrekh <viktorhrekh@viesoft.dev>
2023-03-05 07:45:42 +0200

    Change sonatype host

--- buildSrc/src/main/kotlin/SonatypeRepository.kt
+++ buildSrc/src/main/kotlin/SonatypeRepository.kt
@@ -1,5 +1,5 @@
 object SonatypeRepository {
 
-    const val releasesUrl = "https://oss.sonatype.org/service/local/staging/deploy/maven2/"
-    const val snapshotsUrl = "https://oss.sonatype.org/content/repositories/snapshots/"
+    const val releasesUrl = "https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/"
+    const val snapshotsUrl = "https://s01.oss.sonatype.org/content/repositories/snapshots/"
 }
